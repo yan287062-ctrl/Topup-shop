e21edbcc61730b8e8d867169a83da1875ca119ed
yan287062-ctrl <yan287062@gmail.com>
2026-08-19 23:41:25 +0630

    Configure Supabase connection

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl ='https://aasncvjvjftsyhywrueo.supabase.co'';
+const supabaseAnonKey ='sb_publishable_BWPJnQPpWwysRe84oYfgAw_GinZLV98'';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
