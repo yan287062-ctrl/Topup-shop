e63f471a60038fbe2066cdbe4566b9772a59295a
yan287062-ctrl <yan287062@gmail.com>
2026-08-19 23:46:25 +0630

    Fix syntax error in supabase credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl ='https://aasncvjvjftsyhywrueo.supabase.co'';
-const supabaseAnonKey ='sb_publishable_BWPJnQPpWwysRe84oYfgAw_GinZLV98'';
+const supabaseUrl = `https://aasncvjvjftsyhywrueo.supabase.co`;
+const supabaseAnonKey = `sb_publishable_BWPJnQPpWwysRe84oYfgAw_GinZLV98`;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
