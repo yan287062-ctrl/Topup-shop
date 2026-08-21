3e75db5af921e2309874fe9694655be614296e8d
yan287062-ctrl <yan287062@gmail.com>
2026-08-21 22:26:48 +0630

    Add custom awesome purple gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -442,7 +442,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen animate-bg-gradient text-gray-100 font-sans pb-12">
+    <div className="min-h-screen my-animated-bg   text-gray-100 font-sans pb-12">
       {/* Top Banner */}
       <div className="bg-[#111e33] border-b border-blue-900/40 px-4 py-2 text-xs flex justify-between items-center text-blue-200">
         <span className="font-semibold">🌟 Paing Gyi Game Store - Official Platform</span>
