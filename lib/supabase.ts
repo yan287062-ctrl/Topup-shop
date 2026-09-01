fb42c3ea140d6e4be616df0980674226093a3a17
Zann <yan287062@gmail.com>
2026-09-01 22:17:21 +0630

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lejfhsuwajmzikmudmcs.supabase.co';
+const supabaseUrl = 'https://painggyishop.vercel.app/api/supabase';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlamZoc3V3YWptemlrbXVkbWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjA4NzUsImV4cCI6MjEwMzMzNjg3NX0.x3EVXbqCmrq0yiGlKI6GrWadKWU9TuXKs5F3w8uJNQA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
