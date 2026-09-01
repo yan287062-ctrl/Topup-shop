97ed8d5c0470acf814c49597aebb97369544b297
Zann <yan287062@gmail.com>
2026-09-01 21:58:15 +0630

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/supabase/:path*',
+        destination: 'https://lejfhsuwajmzikmudmcs.supabase.co/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
